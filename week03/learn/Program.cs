869a16f4b8b62a5dae20da156427404ca4bdcd06
Estefania-2023 <estefa20or@gmail.com>
2025-09-17 00:05:31 -0500

    week3

--- week03/learn/Program.cs
+++ week03/learn/Program.cs
@@ -5,9 +5,9 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("\n======================\nDuplicate Counter\n======================");
-        DuplicateCounter.Run();
+        DuplicateCounterSolution.Run();
 
         Console.WriteLine("\n======================\nTranslator\n======================");
-        Translator.Run();
+        TranslatorSolution.Run();
     }
 }
